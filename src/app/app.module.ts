195a25c8311862f4051ad1722ab3444da735e885
AtsignSina <atsignsina@gmail.com>
2019-12-29 19:51:56 +0330

    complete main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import {AppComponent} from './app.component';
 import {LocalStorageService} from './local-storage/local-storage.service';
 import {MainComponent} from './main/main.component';
 import {WizardBaseComponent} from './wizard/wizard-base/wizard-base.component';
+import {WizardDetailComponent} from './wizard/wizard-detail/wizard-detail.component';
+import {WizardSettingComponent} from './wizard/wizard-setting/wizard-setting.component';
+import {WizardDeliveryComponent} from './wizard/wizard-delivery/wizard-delivery.component';
+import {WizardAcceptComponent} from './wizard/wizard-accept/wizard-accept.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {WizardService} from './wizard/wizard.service';
 import {StoreModule} from '@ngrx/store';
@@ -13,12 +17,13 @@ import {projectReducer} from './project/project.reducer';
 import {EffectsModule} from '@ngrx/effects';
 import {ProjectEffect} from './project/project.effect';
 import {ProjectService} from './project/project.service';
+import {Ng2SmartTableModule} from 'ng2-smart-table';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    WizardBaseComponent
+    WizardBaseComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,7 +32,8 @@ import {ProjectService} from './project/project.service';
     StoreModule.forRoot({
       project: projectReducer
     }),
-    EffectsModule.forRoot([ProjectEffect])
+    EffectsModule.forRoot([ProjectEffect]),
+    Ng2SmartTableModule,
   ],
   providers: [LocalStorageService, ProjectService, WizardService],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Project} from '../project/project.proto';
+import {ProjectService} from '../project/project.service';
+import {Store} from '@ngrx/store';
+import {Router} from '@angular/router';
+import {ProjectDeleteAction, ProjectEditAction} from '../project/project.actions';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +12,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  projects: any[] = [];
+  setting = {
+    mode: 'external',
+    columns: {
+      name: {
+        title: 'Name'
+      },
+      owner: {
+        title: 'Owner'
+      },
+      customer: {
+        title: 'Customer'
+      },
+      phone: {
+        title: 'Contact Phone'
+      },
+      email: {
+        title: 'Address'
+      },
+      website: {
+        title: 'Website'
+      }
+    }
+  };
+
+  private static simplify(project: Project) {
+    return {
+      uuid: project.uuid,
+      name: project.details.projectName,
+      owner: project.details.projectOwner,
+      customer: project.details.customerName,
+      phone: project.details.contactPhone,
+      email: project.details.emailAddress,
+      website: project.details.companySite
+    };
+  }
+
+  constructor(private projectService: ProjectService, private store: Store<any>, private router: Router) {
+  }
 
   ngOnInit() {
+    this.list();
+    console.log(this.projects);
+  }
+
+
+  delete($event: any) {
+    this.store.dispatch(new ProjectDeleteAction($event.data.uuid));
+    this.list();
+  }
+
+
+  edit($event: any) {
+    this.store.dispatch(new ProjectEditAction(this.projectService.fetch($event.data.uuid)));
+  }
+
+  private list() {
+    this.projects = this.projectService.list().map(p => {
+      return MainComponent.simplify(p);
+    });
+  }
+
+  add($event: any) {
+    this.router.navigate(['/wizard']);
   }
 
 }
